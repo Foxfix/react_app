ce8f7992950cc5120b8ac0de2c112281e335ee97
olga <test@test.com>
2018-09-26 21:25:10 +0300

    higher order component

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,5 +1,7 @@
 import React, {Component} from 'react';
-import classes from './Person.css'
+import classes from './Person.css';
+import Aux from '../../../hoc/Aux';
+import withClass from '../../../hoc/withClass';
 
 class Person extends Component {
     constructor(props) {
@@ -17,12 +19,12 @@ class Person extends Component {
     render() {
         console.log('[Person.js] Inside render()');
         return (
-            <div className={classes.Person}>
+            <Aux>
                 <p onClick={this.props.click}>I'm a person {this.props.name}! I am {this.props.age} years old!</p>
                 <p>{this.props.children}</p>
                 <input type="text" onChange={this.props.change} value={this.props.name}/>
-            </div>
+            </Aux>
         )
     }
 }
-export default Person;
+export default withClass(Person, classes.Person);
